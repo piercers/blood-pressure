523cf524f17c841dd521a9fcdc11d7ffd5f2a11d
Ryan Pierce <ry@nspierce.com>
2020-05-23 01:29:52 -0600

    Add Chart.js.

--- src/views/Entries.vue
+++ src/views/Entries.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
+    <h1>Blood Pressure</h1>
     <section>
-      <h1>Entries</h1>
+      <!-- TODO Move to own component (pass entries via props) -->
+      <canvas ref="myChart"></canvas>
+    </section>
+    <section>
+      <h2>Entries</h2>
       <ul>
         <li v-for="entry of entries" v-bind:key="entry.dateTime">{{ entry }}</li>
       </ul>
@@ -14,10 +19,16 @@
 </template>
 
 <script lang="ts">
+// TODO Not sure why this is erroring
+import { Chart } from "chart.js";
 import { Component, Vue } from "vue-property-decorator";
 
 import AddEntry from "@/components/AddEntry.vue";
 import { Entry } from "@/core/entries.interfaces";
+
+const randomScalingFactor = function() {
+  return Math.round(Math.random() * 100);
+};
 
 @Component({
   components: {
@@ -25,7 +36,96 @@ import { Entry } from "@/core/entries.interfaces";
   }
 })
 export default class Entries extends Vue {
+  $refs!: {
+    myChart: HTMLCanvasElement;
+  };
+
+  mounted() {
+    this.chart();
+  }
+
   entries: Entry[] = [];
+
+  chart() {
+    new Chart(this.$refs.myChart, {
+      type: "line",
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "#ff0000",
+            borderColor: "#ff0000",
+            data: [
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor()
+            ],
+            fill: false
+          },
+          {
+            label: "My Second dataset",
+            fill: false,
+            backgroundColor: "blue",
+            borderColor: "blue",
+            data: [
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor(),
+              randomScalingFactor()
+            ]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: "Chart.js Line Chart"
+        },
+        tooltips: {
+          mode: "index",
+          intersect: false
+        },
+        hover: {
+          mode: "nearest",
+          intersect: true
+        },
+        scales: {
+          // TODO This is throwing a typing error
+          x: {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: "Month"
+            }
+          },
+          y: {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: "Value"
+            }
+          }
+        }
+      }
+    });
+  }
 
   addEntry(entry: Entry) {
     this.entries.push(entry);
