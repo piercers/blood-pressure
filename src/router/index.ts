45fd84e2d80acb97c7b739bacbcdd5471eb870e4
Ryan Pierce <ry@nspierce.com>
2020-06-02 22:00:14 -0600

    Require auth on routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,21 @@
 import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
+import VueRouter, { RouteConfig, NavigationGuard } from "vue-router";
 
 import AddEntry from "@/views/AddEntry.vue";
 import Entries from "@/views/Entries.vue";
 import Graph from "@/views/Graph.vue";
 import SignIn from "@/views/SignIn.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
+
+const requireAuth: NavigationGuard = (to, from, next) => {
+  if (store.state.user) {
+    next();
+  } else {
+    next("/sign-in");
+  }
+};
 
 export const routes: Array<RouteConfig> = [
   {
@@ -14,40 +23,43 @@ export const routes: Array<RouteConfig> = [
     name: "Graph",
     component: Graph,
     meta: {
-      inNav: true,
+      inNav: true
     },
+    beforeEnter: requireAuth
   },
   {
     path: "/add",
     name: "Add",
     component: AddEntry,
     meta: {
-      inNav: true,
+      inNav: true
     },
+    beforeEnter: requireAuth
   },
   {
     path: "/list",
     name: "List",
     component: Entries,
     meta: {
-      inNav: true,
+      inNav: true
     },
+    beforeEnter: requireAuth
   },
   {
     path: "/sign-in",
     name: "Sign In",
-    component: SignIn,
+    component: SignIn
   },
   {
     path: "*",
-    redirect: "/graph",
-  },
+    redirect: "/graph"
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
