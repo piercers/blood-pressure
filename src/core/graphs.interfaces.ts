6d82ab7eb578b765503fd4cf5442e48e3617fdcd
Ryan Pierce <ry@nspierce.com>
2020-06-12 23:55:37 -0600

    Remove note about parsing down ChartJS's API surface.

--- src/core/graphs.interfaces.ts
+++ src/core/graphs.interfaces.ts
@@ -1,6 +1,5 @@
 import { ChartPoint } from "chart.js";
 
-// TODO Determine if these "aliases" are needed when ChartJS provides them
 interface DataSet {
   backgroundColor: string;
   borderColor: string;
