1a602456710478c05ff5381d955c0e77d2887237
Ryan Pierce <ry@nspierce.com>
2020-06-18 20:59:32 -0600

    Padding to profile.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
+  <div class="container">
     <h2>Account</h2>
     <p>{{ email }}</p>
     <button v-on:click="signOut" class="app-button">Sign Out</button>
   </div>
 </template>
+
+<style scoped>
+.container {
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+</style>
 
 <script lang="ts">
 import { Component, Vue, Watch, Prop } from "vue-property-decorator";
